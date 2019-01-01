0333749d01f7b595c0441f0b86368341639e5c86
chengwill97 <chengwill97@gmail.com>
2019-01-01 01:46:31 -0600

    View text file names uploaded

--- app/static/js/files.js
+++ app/static/js/files.js
@@ -22,3 +22,28 @@ $(function() {
 
     });
 });
+
+$(function() {
+    $('#inputText').change(function(){
+
+        if (this.files.length == 0) {
+            $('.textNameScroll').hide();
+        } else {
+            $('.textNameScroll').show();
+        }
+
+        $('#textNames').html('');
+
+        $('#textNames').append('<li class="list-group-item active d-flex justify-content-between align-items-center"> Text Files Uploaded <span class="badge badge-secondary badge-pill">'
+        + this.files.length
+        + '</span></li>');
+
+        for(var i = 0 ; i <= this.files.length ; i++){
+            if(typeof this.files[i] !== 'undefined') {
+                var filename = this.files[i].name;
+                $('#textNames').append('<li class="list-group-item">' + filename + '</li>');
+            }
+        }
+
+    });
+});
